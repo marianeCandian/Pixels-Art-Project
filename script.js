ac6670c4fe40fdc481a2006a396e3eff977ba7f3
Mariane Candian <mariane.candian@gmail.com>
2022-08-02 17:19:01 -0300

    Requisitos 5 e 6

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+  function selectedClass() {
+    const corPreta = document.getElementById('color1');
+    corPreta.classList.add('selected');
+  }
+  selectedClass();
+}
