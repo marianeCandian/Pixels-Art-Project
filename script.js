7d6758c5746742a5ed3dd6845d116db778608c9a
Mariane Candian <mariane.candian@gmail.com>
2022-08-03 11:03:37 -0300

    Requisito 7 em andamento

--- script.js
+++ script.js
@@ -1,7 +1,22 @@
-window.onload = function () {
-  function selectedClass() {
-    const corPreta = document.getElementById('color1');
-    corPreta.classList.add('selected');
-  }
-  selectedClass();
+function selectedClass() {
+  const corPreta = document.getElementById('color1');
+  corPreta.classList.add('selected');
 }
+selectedClass();
+
+function selecionandoCor() {
+  const getElement = document.querySelectorAll('.color');
+  const getElementeSelected = document.querySelector('.selected');
+  getElementeSelected.classList.remove('selected');
+  for (let index = 0; index < getElement.length; index += 1) {
+    if (getElement[index].classList === 'selected') {
+      getElement[index].classList.remove('selected');
+    } else {
+      getElement[index].addEventListener('click', function (event) {
+        event.target.classList.add('selected');
+      });
+    }
+  }
+}
+
+selecionandoCor();
